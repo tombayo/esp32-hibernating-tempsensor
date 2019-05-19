219f28be39cc6558e04524b91bee4c1da291c6f1
tombayo <github@tombayo.com>
2019-05-19 16:47:08 +0200

    Working in the DS18B20 temp sensor

--- main/app_main.c
+++ main/app_main.c
@@ -38,32 +38,158 @@
 #include "owb_rmt.h"
 #include "ds18b20.h"
 
+static const char *WIFITAG = "WIFI_STATION";
 #define ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
 #define ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
 #define ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
-/* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
-/* The event group allows multiple bits for each event, but we only care about one event 
- * - are we connected to the AP with an IP? */
 const int WIFI_CONNECTED_BIT = BIT0;
-static const char *WIFITAG = "WIFI_STATION";
 static int s_retry_num = 0;
 
-
+static const char *HTTPTAG = "HTTP_CLIENT";
 #define MAX_HTTP_RECV_BUFFER 512
 #define HTTP_POST_URL CONFIG_HTTP_URL
-static const char *HTTPTAG = "HTTP_CLIENT";
 
+static const char *SLEEPTAG = "SLEEP_WAKEUP";
 #define WAKEUP_TIMEOUT  CONFIG_WAKEUP_TIMEOUT
 static RTC_DATA_ATTR struct timeval sleep_enter_time;
-static const char *SLEEPTAG = "SLEEP_WAKEUP";
 
+static const char *TEMPTAG = "1W_TEMPSENSOR";
 #define GPIO_DS18B20_0      CONFIG_ONE_WIRE_GPIO
 #define MAX_DEVICES         8
 #define DS18B20_RESOLUTION  DS18B20_RESOLUTION_12_BIT
 #define SAMPLE_PERIOD       1000 // ms
-static const char *TEMPTAG = "1W_TEMPSENSOR";
 
+
+
+static void read_temperature_sensor() {
+ 
+  // Stable readings require a brief period before communication
+  vTaskDelay(2000.0 / portTICK_PERIOD_MS);
+
+  // Create a 1-Wire bus, using the RMT timeslot driver
+  OneWireBus *owb;
+  owb_rmt_driver_info rmt_driver_info;
+  owb = owb_rmt_initialize(&rmt_driver_info, GPIO_DS18B20_0, RMT_CHANNEL_1, RMT_CHANNEL_0);
+  owb_use_crc(owb, true);  // enable CRC check for ROM code
+
+  // Find all connected devices
+  ESP_LOGI(TEMPTAG,"Find devices:\n");
+  OneWireBus_ROMCode device_rom_codes[MAX_DEVICES] = {0};
+  int num_devices = 0;
+  OneWireBus_SearchState search_state = {0};
+  bool found = false;
+  owb_search_first(owb, &search_state, &found);
+  while (found) {
+    char rom_code_s[17];
+    owb_string_from_rom_code(search_state.rom_code, rom_code_s, sizeof(rom_code_s));
+    printf("  %d : %s\n", num_devices, rom_code_s);
+    device_rom_codes[num_devices] = search_state.rom_code;
+    ++num_devices;
+    owb_search_next(owb, &search_state, &found);
+  }
+  ESP_LOGI(TEMPTAG, "Found %d device%s\n", num_devices, num_devices == 1 ? "" : "s");
+
+  // In this example, if a single device is present, then the ROM code is probably
+  // not very interesting, so just print it out. If there are multiple devices,
+  // then it may be useful to check that a specific device is present.
+
+  if (num_devices == 1) {
+    // For a single device only:
+    OneWireBus_ROMCode rom_code;
+    owb_status status = owb_read_rom(owb, &rom_code);
+    if (status == OWB_STATUS_OK) {
+      char rom_code_s[OWB_ROM_CODE_STRING_LENGTH];
+      owb_string_from_rom_code(rom_code, rom_code_s, sizeof(rom_code_s));
+      ESP_LOGI(TEMPTAG, "Single device %s present\n", rom_code_s);
+    }
+    else {
+      ESP_LOGI(TEMPTAG, "An error occurred reading ROM code: %d", status);
+    }
+  }
+  else {
+    // Search for a known ROM code (LSB first):
+    // For example: 0x1502162ca5b2ee28
+    OneWireBus_ROMCode known_device = {
+      .fields.family = { 0x28 },
+      .fields.serial_number = { 0xee, 0xb2, 0xa5, 0x2c, 0x16, 0x02 },
+      .fields.crc = { 0x15 },
+    };
+    char rom_code_s[OWB_ROM_CODE_STRING_LENGTH];
+    owb_string_from_rom_code(known_device, rom_code_s, sizeof(rom_code_s));
+    bool is_present = false;
+
+    owb_status search_status = owb_verify_rom(owb, known_device, &is_present);
+    if (search_status == OWB_STATUS_OK) {
+      ESP_LOGI(TEMPTAG, "Device %s is %s\n", rom_code_s, is_present ? "present" : "not present");
+    }
+    else {
+      ESP_LOGI(TEMPTAG, "An error occurred searching for known device: %d", search_status);
+    }
+  }
+
+  // Create DS18B20 devices on the 1-Wire bus
+  DS18B20_Info * devices[MAX_DEVICES] = {0};
+  for (int i = 0; i < num_devices; ++i) {
+    DS18B20_Info * ds18b20_info = ds18b20_malloc();  // heap allocation
+    devices[i] = ds18b20_info;
+
+    if (num_devices == 1) {
+      ESP_LOGI(TEMPTAG, "Single device optimisations enabled\n");
+      ds18b20_init_solo(ds18b20_info, owb);          // only one device on bus
+    }
+    else {
+      ds18b20_init(ds18b20_info, owb, device_rom_codes[i]); // associate with bus and device
+    }
+    ds18b20_use_crc(ds18b20_info, true);           // enable CRC check for temperature readings
+    ds18b20_set_resolution(ds18b20_info, DS18B20_RESOLUTION);
+  }
+
+  // Read temperatures more efficiently by starting conversions on all devices at the same time
+  int errors_count[MAX_DEVICES] = {0};
+  int sample_count = 0;
+  if (num_devices > 0) {
+    TickType_t last_wake_time = xTaskGetTickCount();
+
+    while (1) {
+      last_wake_time = xTaskGetTickCount();
+
+      ds18b20_convert_all(owb);
+
+      // In this application all devices use the same resolution,
+      // so use the first device to determine the delay
+      ds18b20_wait_for_conversion(devices[0]);
+
+      // Read the results immediately after conversion otherwise it may fail
+      // (using printf before reading may take too long)
+      float readings[MAX_DEVICES] = { 0 };
+      DS18B20_ERROR errors[MAX_DEVICES] = { 0 };
+
+      for (int i = 0; i < num_devices; ++i) {
+        errors[i] = ds18b20_read_temp(devices[i], &readings[i]);
+      }
+
+      // Print results in a separate loop, after all have been read
+      ESP_LOGI(TEMPTAG, "\nTemperature readings (degrees C): sample %d\n", ++sample_count);
+      for (int i = 0; i < num_devices; ++i) {
+        if (errors[i] != DS18B20_OK) {
+          ++errors_count[i];
+        }
+
+        ESP_LOGI(TEMPTAG, "  %d: %.1f    %d errors\n", i, readings[i], errors_count[i]);
+      }
+
+      vTaskDelayUntil(&last_wake_time, SAMPLE_PERIOD / portTICK_PERIOD_MS);
+    }
+  }
+
+  // clean up dynamically allocated data
+  for (int i = 0; i < num_devices; ++i) {
+    ds18b20_free(&devices[i]);
+  }
+
+  owb_uninitialize(owb);
+}
 
 /**
  * Sends the ESP32 off to sleep.
@@ -140,13 +266,18 @@ static void http_post_data(char *post_data) {
   esp_http_client_cleanup(client);
 }
 
-
+/**
+ * Called when Wifi and IP-address is OK.
+ * */
 static void connected_task(void *pvParameters) {
+
+
+
   http_post_data("field1=value1&field2=value2");
 
-  hibernate();
-
   vTaskDelete(NULL);
+
+  hibernate();
 }
 
 /**
@@ -233,6 +364,7 @@ void app_main() {
   }
   ESP_ERROR_CHECK(ret);
 
+  read_temperature_sensor();
   
-  wifi_init_sta(); // Runs connected_task() once everything is up an running
+  //wifi_init_sta(); // Runs connected_task() once everything is up an running
 }
